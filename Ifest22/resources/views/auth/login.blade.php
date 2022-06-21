deb31ee5cb559318cc6a2f2f3b6572106f16959d
rhezapandya <rheza20001@mail.unpad.ac.id>
2022-06-21 18:38:39 +0700

    update page login : font dkk sesuai mockup

--- Ifest22/resources/views/auth/login.blade.php
+++ Ifest22/resources/views/auth/login.blade.php
@@ -10,6 +10,7 @@
     <!-- CSS Files -->
     <link rel="stylesheet" href="{{ asset('atlantis/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('atlantis/css/atlantis.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/login.css') }}">
 </head>
 
 <body class="login">
@@ -21,8 +22,8 @@
         </div>
         <div class="login-aside w-50 d-flex align-items-center justify-content-center bg-white">
             <div class="container container-login container-transparent animated fadeIn">
-                <h3 class="text-center">Hello!</h3>
-                <h4 class="text-center">Sign into Your Account</h4>
+                <p class="text-center text-greeting">Hello!</p>
+                <p class="text-center text-greeting-desc">Sign into Your Account</p>
 
                 <form action="{{ route('login') }}" method="post" autocomplete="off">
 
@@ -30,8 +31,8 @@
 
                     <div class="login-form">
                         <div class="form-group">
-                            <label for="email" class="placeholder">{{ __('Email Address') }}</label>
-                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            <label for="email" class="placeholder text-form">{{ __('Email Address') }}</label>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror ifest-form" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                             @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
@@ -39,10 +40,10 @@
                             @enderror
                         </div>
                         <div class="form-group">
-                            <label for="password" class="placeholder">{{ __('Password') }}</label>
-                            <!--<a href="{{ route('password.request') }}" class="link float-right">Lupa Password ?</a>-->
+                            <label for="password" class="placeholder text-form">{{ __('Password') }}</label>
+                            
                             <div class="position-relative">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror ifest-form" name="password" required autocomplete="current-password">
                                 <div class="show-password">
                                     <i class="icon-eye"></i>
                                 </div>
@@ -50,13 +51,16 @@
                         </div>
                         <div class="form-group form-action-d-flex mb-3">
                             <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                <label class="custom-control-label m-0" for="rememberme">{{ __('Remember Me') }}</label>
+                                <div class="position-relative">
+                                    <input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                </div>
+                                <label class="custom-control-label m-0 text-remember-me" for="rememberme">{{ __('Remember Me') }}</label>
                             </div>
+                            <a href="{{ route('password.request') }}" class="link float-right text-forgot-password">Forgot Password?</a>
                         </div>
                         <div class="form-group form-action-d-flex mb-3">
-                            <button type="submit" class="btn col-md-5 float-right mt-3 mt-sm-0 fw-bold" style="background-color:white; border-color:black; color:black;">{{ __('Login') }}</button>
-                            <button class="btn col-md-5 float-right mt-3 mt-sm-0 fw-bold" style="background-color:white; border-color:black; color:black;"><a href="{{route('register')}}" style="text-decoration:none; color:#1c1c1c;">Register</a></button>
+                            <button type="submit" class="btn btn-primary ifest-btn-primary col-md-5 float-right mt-3 mt-sm-0 fw-bold">{{ __('Login') }}</button>
+                            <button class="btn btn-outline-primary ifest-btn-outline-primary col-md-5 float-right mt-3 mt-sm-0 fw-bold"><a class="ifest-btn-outline-primary-text" href="{{route('register')}}">Register</a></button>
                         </div>
                     </div>
                 </form>
