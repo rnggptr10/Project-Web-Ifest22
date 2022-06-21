cefcb9ce02a9eac47014528863f3bb0f6da85376
dapioo <athallahdavio@gmail.com>
2022-06-21 15:38:08 +0700

    update login

--- Ifest22/resources/views/auth/login.blade.php
+++ Ifest22/resources/views/auth/login.blade.php
@@ -1,69 +1,73 @@
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<!DOCTYPE html>
+<html lang="en">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Login {{ config('app.name') }}</title>
+    <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+    <link rel="icon" href="{{ asset('img/favicon.ico') }}" type="image/x-icon" />
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="{{ asset('atlantis/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('atlantis/css/atlantis.css') }}">
+</head>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+<body class="login">
+    <div class="wrapper wrapper-login wrapper-login-full p-0" style="background-color: #1c1c1c;">
+        <div class="login-aside w-50 d-flex flex-column align-items-center justify-content-center text-center">
+            <div>
+                <img src="{{ asset('img/logo/logopurple.png') }}" alt="" style="width: 60%; height:auto;">
+            </div>
+        </div>
+        <div class="login-aside w-50 d-flex align-items-center justify-content-center bg-white">
+            <div class="container container-login container-transparent animated fadeIn">
+                <h3 class="text-center">Hello!</h3>
+                <h4 class="text-center">Sign into Your Account</h4>
 
-                                @error('email')
+                <form action="{{ route('login') }}" method="post" autocomplete="off">
+
+                    @csrf
+
+                    <div class="login-form">
+                        <div class="form-group">
+                            <label for="email" class="placeholder">{{ __('Email Address') }}</label>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                            @error('email')
                                 <span class="invalid-feedback" role="alert">
                                     <strong>{{ $message }}</strong>
                                 </span>
-                                @enderror
-                            </div>
+                            @enderror
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="password" class="placeholder">{{ __('Password') }}</label>
+                            <!--<a href="{{ route('password.request') }}" class="link float-right">Lupa Password ?</a>-->
+                            <div class="position-relative">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                                <div class="show-password">
+                                    <i class="icon-eye"></i>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-                                <a href="{{route('register')}}">Register</a>
-                                @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                                @endif
+                        <div class="form-group form-action-d-flex mb-3">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <label class="custom-control-label m-0" for="rememberme">{{ __('Remember Me') }}</label>
                             </div>
                         </div>
-                    </form>
-                </div>
+                        <div class="form-group form-action-d-flex mb-3">
+                            <button type="submit" class="btn col-md-5 float-right mt-3 mt-sm-0 fw-bold" style="background-color:white; border-color:black; color:black;">{{ __('Login') }}</button>
+                            <button class="btn col-md-5 float-right mt-3 mt-sm-0 fw-bold" style="background-color:white; border-color:black; color:black;"><a href="{{route('register')}}" style="text-decoration:none; color:#1c1c1c;">Register</a></button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</div>
+    <script src="{{ asset('atlantis/js/core/jquery.3.2.1.min.js') }}"></script>
+    <script src="{{ asset('atlantis/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js') }}"></script>
+    <script src="{{ asset('atlantis/js/core/popper.min.js') }}"></script>
+    <script src="{{ asset('atlantis/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('atlantis/js/atlantis.min.js') }}"></script>
+</body>
+
+</html>
