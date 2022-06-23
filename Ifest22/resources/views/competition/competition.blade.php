2768393a3f2af179113196433f537486356fc658
rhezapandya <rheza20001@mail.unpad.ac.id>
2022-06-23 14:56:23 +0700

    update bikin template competition

---
+++ Ifest22/resources/views/competition/competition.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<title>IFest 2022 - @yield('competition_webtitle')</title>
+    @yield('competition_head')
+
+@extends('layouts.app2')
+
+@section('content')
+<body style="background-color: #1c1c1c;">
+    <div class="container" style="padding: 0; ">
+        <div class="row">
+            <div class="col" style="padding: 20px 0 20px 0;">
+                <div class="row" align="center" id="intDescription" style="padding-bottom: 30px;">
+                    <div style="padding-bottom: 10px;">
+                        <h1 class="text-title">@yield('competition_title')</h1>
+                    </div>
+                    <div style="padding-top: 10px;padding-bottom: 10px;">
+                        <h2 class="text-theme">"@yield('competition_theme')"</h2>
+                    </div>
+                    <div style="padding: 10px 50px 0 50px; font-size: 16px;">
+                        <p class="text-desc">@yield('competition_desc')</p>
+                    </div>
+                </div>
+
+                @yield('competition_flashback')
+
+                <div class="row" align="center" id="competitionPrizes" style="padding-top: 30px;padding-bottom: 30px;">
+                    <h1 class="text-sub-title">Prizes</h1>
+                    <div class="row justify-content-center" style="padding-top: 20px;"">
+                            <div class=" col-2">
+                        <img style="width:100%;height: 160px;" src="https://clipground.com/images/trophy-winner-clipart-5.jpg">
+                        <p style="color:white">@yield('first_prize')</p>
+                    </div>
+                    <div class="col-2">
+                        <img style="width:100%;height: 180px;" src="https://clipground.com/images/trophy-winner-clipart-5.jpg">
+                        <p style="color:white">@yield('second_prize')</p>
+                    </div>
+                    @yield('third_prize')
+                </div>
+            </div>
+
+            @yield('competition_judges')
+
+            <div class="row" align="center" id="competitionTimeline" style="padding-top: 30px;padding-bottom: 30px;">
+                <h1 class="text-sub-title">Timeline</h1>
+                @yield('competition_timeline')
+            </div>
+
+            <div class="row justify-content-center align-items-center" align="center" id="competitionButton" style="padding-top: 30px;padding-bottom: 30px;">
+                @yield('competition_button')
+            </div>
+
+            <div class="row justify-content-center align-items-center" align="center" id="competitionFAQ" style="padding-top: 30px;padding-bottom: 30px;">
+                <h1 class="text-sub-title">Frequently Asked Question (FAQ)</h1>
+                @yield('competition_faq')
+            </div>
+            
+            <div class="row justify-content-center align-items-center" align="center" id="competitionCP" style="padding-top: 30px;padding-bottom: 30px;">
+                <h1 class="text-sub-title">Contact Person</h1>
+                <div class="col-2" style="padding-top: 30px;">
+                    <button type="button" class="btn btn-outline-primary ifest-btn-outline-primary-dark-bg" style="width: 147px;height: 48px;" onclick="window.location.href='https://wa.me/@yield('cp1_wa')';" disabled>
+                        <img class="logo-whatsapp" src="{{ URL::asset('icon/whatsapp.svg') }}" alt="whatsapp">
+                        <span>@yield('cp1_name')</span>
+                    </button>
+                </div>
+                <div class="col-2" style="padding-top: 30px;">
+                    <button type="button" class="btn btn-outline-primary ifest-btn-outline-primary-dark-bg" style="width: 147px;height: 48px;" onclick="window.location.href='https://wa.me/@yield('cp2_wa')';" disabled>
+                        <img class="logo-whatsapp" src="{{ URL::asset('icon/whatsapp.svg') }}" alt="whatsapp">
+                        <span>@yield('cp2_name')</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+@endsection
--- Ifest22/resources/views/competition/dataAnalysis.blade.php
+++ Ifest22/resources/views/competition/dataAnalysis.blade.php
@@ -1,12 +1,140 @@
-@extends('layouts.app')
-@section('content')
-<p>Halaman DAC</p>
-<a href="">view Guidebook</a>
-@guest
-@if(Route::has('login'))
-<a href="{{route('register')}}">Register Now</a>
-@endif
-@else
-<a href="{{route('dac.registration.view')}}">Register Now</a>
-@endguest
+@extends('competition.competition')
+
+@section('competition_head')
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+<link rel="stylesheet" href="{{ URL::asset('css/generalStyle.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('css/competitions.css') }}">
 @endsection
+
+@section('competition_webtitle', 'Data Analysis Competition')
+
+@section('competition_title', 'Data Analysis Competition')
+
+@section('competition_theme', 'Tema')
+
+@section('competition_desc')
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+@endsection
+
+@section('competition_flashback')
+<div class="row align-items-center" align="center" id="intFlashback" style="padding-top: 30px;padding-bottom: 30px;">
+    <div class="col-1">
+        <img class="arrow-switch" src="{{ URL::asset('icon/circle-arrow-left.svg') }}" alt="arrow-left">
+    </div>
+    <div class="col-10">
+        <div class="row">
+            <div class="col-sm" align="center" style="padding-right: 25px;">
+                <img style="width:100%;height: 320px;" src="https://cf.shopee.co.id/file/e0d319d464c87407718586d008a144b7">
+            </div>
+            <div class="col-sm" align="center" style="padding-right: 25px;padding-left: 25px;">
+                <img style="width:100%;height: 320px;" src="https://cf.shopee.co.id/file/e0d319d464c87407718586d008a144b7">
+            </div>
+            <div class="col-sm" align="center" style="padding-left: 25px;">
+                <img style="width:100%;height: 320px;" src="https://cf.shopee.co.id/file/e0d319d464c87407718586d008a144b7">
+            </div>
+        </div>
+    </div>
+    <div class="col-1">
+        <img class="arrow-switch" src="{{ URL::asset('icon/circle-arrow-right.svg') }}" alt="arrow-right">
+    </div>
+</div>
+@endsection
+
+@section('first_prize', 'Rp.3.000.000')
+@section('second_prize', 'Rp.2.000.000')
+@section('third_prize')
+<div class="col-2">
+    <img style="width:100%;height: 160px;" src="https://clipground.com/images/trophy-winner-clipart-5.jpg">
+    <p style="color:white">Rp. 1.000.000</p>
+</div>
+@endsection
+
+@section('competition_judges')
+<div class="row" align="center" id="intJudges" style="padding-top: 30px;padding-bottom: 30px;">
+    <h1 class="text-sub-title">Our Judges</h1>
+    <div class="row justify-content-center" style="padding-top: 30px;">
+        <div class="col-4">
+            <div class="card" style="border: 0; width:max-content">
+                <img class="card-img-top" style="width:100%;height:350px;" src="https://cf.shopee.co.id/file/e0d319d464c87407718586d008a144b7" alt="Card image cap">
+                <div class="row card-body justify-content-center align-items-center" style="padding: 20px 0 20px 0;">
+                    <div class="col-7" style="line-height: 5px;">
+                        <h5 style="color:black">Wildan Wortel</h5>
+                        <p style="color:black">Pejabat Negara</p>
+                    </div>
+                    <div class="col-3">
+                        <img class="logo-linkedin" src="{{ URL::asset('icon/linkedin.svg') }}" alt="linkedin">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="card" style="border: 0; width:max-content">
+                <img class="card-img-top" style="width:100%;height:350px;" src="https://cf.shopee.co.id/file/e0d319d464c87407718586d008a144b7" alt="Card image cap">
+                <div class="row card-body justify-content-center align-items-center" style="padding: 20px 0 20px 0;">
+                    <div class="col-7" style="line-height: 5px;">
+                        <h5 style="color:black">Wildan Wortel</h5>
+                        <p style="color:black">Pejabat Negara</p>
+                    </div>
+                    <div class="col-3">
+                        <img class="logo-linkedin" src="{{ URL::asset('icon/linkedin.svg') }}" alt="linkedin">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('competition_timeline')
+<div class="row" style="padding-top: 30px;">
+    <p style="color:white;">timeline bulet2 lah pokoknya</p>
+</div>
+@endsection
+
+@section('competition_button')
+<div class="col-2">
+    <button type="button" class="btn btn-outline-primary ifest-btn-outline-primary-dark-bg" style="width: 147px;height: 48px;">Guidebook</button>
+</div>
+<div class="col-2">
+    <button type="button" class="btn btn-primary ifest-btn-primary-dark-bg" style="width: 147px;height: 48px;">
+        @guest
+        @if(Route::has('login'))
+        <a class="ifest-btn-primary-dark-bg-text" href="{{route('login')}}">Register Now</a>
+        @endif
+        @else
+        <a class="ifest-btn-primary-dark-bg-text" href="{{route('dac.registration.view')}}">Register Now</a>
+        @endguest
+    </button>
+</div>
+@endsection
+
+@section('competition_faq')
+<div class="row" style="padding-top: 30px;">
+    <p style="color:white;">ini FAQ</p>
+    <!-- <div class="card">
+        <details style="color:#000000;">
+            <summary>Pertanyaan 1</summary>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                unknown</p>
+        </details>
+    </div>
+    <div class="card">
+        <details style="color:#000000;">
+            <summary>Pertanyaan 1</summary>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                unknown</p>
+        </details>
+    </div> -->
+</div>
+@endsection
+
+@section('cp1_wa', '6287720201166')
+@section('cp1_name', 'Jarwo 1')
+@section('cp2_wa', '6287720201177')
+@section('cp2_name', 'Jarwo 2')
