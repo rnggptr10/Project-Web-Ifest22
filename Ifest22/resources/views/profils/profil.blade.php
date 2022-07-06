f09b62d31764cd9a8e9c889ee1f815d0ffb78344
dapioo <athallahdavio@gmail.com>
2022-07-06 21:37:37 +0700

    update

--- Ifest22/resources/views/profils/profil.blade.php
+++ Ifest22/resources/views/profils/profil.blade.php
@@ -26,7 +26,7 @@
 @section('content')
 <body style="background-color: #1c1c1c;">
     <div class="container" style="padding: 0; padding-bottom:50px; padding-top:80px;">
-        <div class="row justify-content-between">
+        <div class="row justify-content-between" style="width: 100%; margin-left: 0;">
             <div class="col-sm-6">
                 <div class="card" style="padding:20px; background-color:#2D2D2D">
                     <div class="row" align="center" style="padding-bottom:15px;">
