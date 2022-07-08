644ece5794547afc0722ad0e896766c2a3d5764a
dapioo <athallahdavio@gmail.com>
2022-07-09 01:14:41 +0700

    ipod

--- Ifest22/resources/views/events/ipod.blade.php
+++ Ifest22/resources/views/events/ipod.blade.php
@@ -10,6 +10,7 @@
 <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ URL::asset('css/events.css') }}">
 <link rel="stylesheet" href="{{ URL::asset('css/landingpage.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('css/ipod.css') }}">
 
 @extends('layouts.app')
 
@@ -21,29 +22,38 @@
 @section('content')
 <body style="background-color: #1c1c1c;">
     <div class="container" style="padding: 100px 0 50px 0;">
-        <div class="row">
-            <div class="col" style="padding: 20px 0 20px 0;">
+        <div class="row justify-content-center">
+            <div class="col-sm-5" style="padding: 20px 0 20px 0; text-align:center;">
+                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAGFBMVEXBw8Ta183y8e74+Pm9v8Dp5+H///+5u7zLiu2GAAABH0lEQVR4nO3PCQ2EABAAMX78Oz4IORGzaR10Wadb1nOb7HyG27XPdW3vcL+Pqe79Gx7LVIdhnmGfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYd9/eB9T3d/w2ue63uG5TXY+w+l+h1EihcbqknUAAAAASUVORK5CYII=" style="width: 70%;">
+            </div>
+            <div class="col-sm-7" style="padding: 20px 0 20px 0;">
                 <div class="row" align="center" id="eventDescription" style="padding-bottom: 30px;">
-                    <div style="padding-bottom: 10px;">
-                        <h1 class="text-title">IFest Podcast (iPod)</h1>
+                    <div style="padding: 10px 50px 0 50px; text-align:left;">
+                        <h1 class="text-title" style="margin: 0 0 0 0;">iPod</h1>
+                        <p class="text-desc" style="margin: 0 0 0 0; font-size: 20px;">Informatics Festival<br>Podcast</p>
                     </div>
-
-                    <div style="padding: 10px 50px 0 50px; font-size: 16px;">
+                    <div style="padding: 10px 50px 0 50px; font-size: 16px; text-align:left;">
                         <p class="text-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    </div>
-                </div>
-                <div data-aos="fade-down" data-aos-duration="1000" class="row align-items-center justify-content-center" align="center" id="ipod" style="padding-top: 30px;padding-bottom: 30px;">
-                    <div class="row justify-content-center" style="padding-bottom: 20px;">
-                        <div data-aos="fade-up" data-aos-duration="1000">
-                            <div class="ipod-box">
-                                @yield('podcast_1')
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+        <div class="container-fluid" style="background-color: #2d2d2d;">
+            <div class="row justify-content-center" style="padding-bottom: 20px;">
+                <div data-aos="fade-up" data-aos-duration="1000">
+                    <div class="ipod-box" style="margin-top: 50px; margin-bottom: 30px;">
+                        <p>Podcast</p>
+                    </div>
+                    <div class="ipod-box" style="margin-bottom: 30px;">
+                        <p>Podcast</p>
+                    </div>
+                    <div class="ipod-box" style="margin-bottom: 30px;">
+                        <p>Podcast</p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
     <script>
