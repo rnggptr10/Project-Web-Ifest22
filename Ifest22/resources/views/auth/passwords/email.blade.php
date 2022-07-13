b24aa39012e1acf1a185072310ef6a4037c920e6
rnggptr10 <ranggaputra103@gmail.com>
2022-07-13 23:10:27 +0700

    frontend email blade & reset

--- Ifest22/resources/views/auth/passwords/email.blade.php
+++ Ifest22/resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,64 @@
-@extends('layouts.app')
+@extends('registration.regis-competition')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+@section('regist_competition_head')
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+<link rel="stylesheet" href="{{ URL::asset('css/generalStyle.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('css/registration.css') }}">
+@endsection
+@section('regist_competition_title', 'Reset Password')
+@section('regist-competition-form')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<form method="post" action="{{ route('password.email') }}" enctype="multipart/form-data">
+    @csrf
+    <!-- PAGE 1 -->
+    @if (session('status'))
+    <div class="alert alert-success" role="alert">
+        {{ session('status') }}
+    </div>
+    @endif
+    <div id="regist-section-1">
+        <div class="row mb-2">
+            <label for="email" class="placeholder regist-form-text" style="padding:0">{{ __('Email Address') }}</label>
+            <!-- Insert Team Name -->
+            <input id="email" name="email" type="text" class="form-control @error('email') is-invalid @enderror ifest-regist-form text-center" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Insert Your Email Address">
+            @error('email')
+            <div class="alert alert-danger mt-1 d-flex align-items-center" role="alert" style="width: 100%;
+        max-width: 333px; margin: 0 auto">
+                <span class="iconify me-1" data-icon="akar-icons:circle-alert" data-inline="false" style="color: #842029;"></span>
+                {{ $message }}
             </div>
+            @enderror
         </div>
+        <div class="col">
+            <button type="submit" class="btn btn-primary" style="background-color: #8267B4; border:none;">
+                {{ __('Send Password Reset Link') }}
+            </button>
+        </div>
+    </div>
+</form>
+@endsection
+
+@section('cp_registrasi')
+<div class="row justify-content-center align-items-center" align="center" id="eventCP" style="padding-top: 30px;padding-bottom: 30px;">
+    <h1 class="text-sub-title">Contact Person</h1>
+    <div class="col-2" style="padding-top: 30px;">
+        <button type="button" class="btn btn-outline-primary ifest-btn-outline-primary-dark-bg" style="width: 147px;height: 48px;" onclick="window.location.href='https://wa.me/@yield('cp1_wa')';">
+            <img class="logo-whatsapp" src="{{ URL::asset('icon/whatsapp.svg') }}" alt="whatsapp">
+            <span>@yield('cp1_name')</span>
+        </button>
+    </div>
+    <div class="col-2" style="padding-top: 30px;">
+        <button type="button" class="btn btn-outline-primary ifest-btn-outline-primary-dark-bg" style="width: 147px;height: 48px;" onclick="window.location.href='https://wa.me/@yield('cp2_wa')';">
+            <img class="logo-whatsapp" src="{{ URL::asset('icon/whatsapp.svg') }}" alt="whatsapp">
+            <span>@yield('cp2_name')</span>
+        </button>
     </div>
 </div>
 @endsection
+
+@section('cp1_wa', '6287720201166')
+@section('cp1_name', 'Jarwo 1')
+@section('cp2_wa', '6287720201177')
+@section('cp2_name', 'Jarwo 2')
--- Ifest22/resources/views/auth/passwords/reset.blade.php
+++ Ifest22/resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,81 @@
-@extends('layouts.app')
+@extends('registration.regis-competition')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+@section('regist_competition_head')
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+<link rel="stylesheet" href="{{ URL::asset('css/generalStyle.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('css/registration.css') }}">
+@endsection
+@section('regist_competition_title', 'Reset Password')
+@section('regist-competition-form')
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+<form method="post" action="{{ route('password.update') }}">
+    @csrf
 
-                        <input type="hidden" name="token" value="{{ $token }}">
+    <input type="hidden" name="token" value="{{ $token }}">
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+    <div id="regist-section-1">
+        <div class="row mb-2">
+            <label for="email" class="placeholder regist-form-text" style="padding:0">{{ __('Email Address') }}</label>
+            <!-- Insert Team Name -->
+            <input id="email" name="email" type="text" class="form-control @error('email') is-invalid @enderror ifest-regist-form text-center" value="{{ $email ?? old('email') }}" autocomplete="email" autofocus>
+            @error('email')
+            <div class="alert alert-danger mt-1 d-flex align-items-center" role="alert" style="width: 100%;
+        max-width: 333px; margin: 0 auto">
+                <span class="iconify me-1" data-icon="akar-icons:circle-alert" data-inline="false" style="color: #842029;"></span>
+                {{ $message }}
             </div>
+            @enderror
+            <label for="password" class="placeholder regist-form-text" style="padding:0">{{ __('Password') }}</label>
+            <!-- Insert Team Name -->
+            <input id="password" name="password" type="text" class="form-control @error('password') is-invalid @enderror ifest-regist-form text-center" required autocomplete="new-password" autofocus>
+            @error('password')
+            <div class="alert alert-danger mt-1 d-flex align-items-center" role="alert" style="width: 100%;
+        max-width: 333px; margin: 0 auto">
+                <span class="iconify me-1" data-icon="akar-icons:circle-alert" data-inline="false" style="color: #842029;"></span>
+                {{ $message }}
+            </div>
+            @enderror
+            <label for="password-confirm" class="placeholder regist-form-text" style="padding:0">{{ __('Confirm Password') }}</label>
+            <!-- Insert Team Name -->
+            <input id="password-confirm" name="password_confirmation" type="text" class="form-control @error('password-confirm') is-invalid @enderror ifest-regist-form text-center" required autocomplete="new-confirm" autofocus>
+            @error('password-confirm')
+            <div class="alert alert-danger mt-1 d-flex align-items-center" role="alert" style="width: 100%;
+        max-width: 333px; margin: 0 auto">
+                <span class="iconify me-1" data-icon="akar-icons:circle-alert" data-inline="false" style="color: #842029;"></span>
+                {{ $message }}
+            </div>
+            @enderror
         </div>
+        <div class="col">
+            <button type="submit" class="btn btn-primary" style="background-color: #8267B4; border:none;">
+                {{ __('Reset Password') }}
+            </button>
+        </div>
+    </div>
+</form>
+@endsection
+
+@section('cp_registrasi')
+<div class="row justify-content-center align-items-center" align="center" id="eventCP" style="padding-top: 30px;padding-bottom: 30px;">
+    <h1 class="text-sub-title">Contact Person</h1>
+    <div class="col-2" style="padding-top: 30px;">
+        <button type="button" class="btn btn-outline-primary ifest-btn-outline-primary-dark-bg" style="width: 147px;height: 48px;" onclick="window.location.href='https://wa.me/@yield('cp1_wa')';">
+            <img class="logo-whatsapp" src="{{ URL::asset('icon/whatsapp.svg') }}" alt="whatsapp">
+            <span>@yield('cp1_name')</span>
+        </button>
+    </div>
+    <div class="col-2" style="padding-top: 30px;">
+        <button type="button" class="btn btn-outline-primary ifest-btn-outline-primary-dark-bg" style="width: 147px;height: 48px;" onclick="window.location.href='https://wa.me/@yield('cp2_wa')';">
+            <img class="logo-whatsapp" src="{{ URL::asset('icon/whatsapp.svg') }}" alt="whatsapp">
+            <span>@yield('cp2_name')</span>
+        </button>
     </div>
 </div>
 @endsection
+
+@section('cp1_wa', '6287720201166')
+@section('cp1_name', 'Jarwo 1')
+@section('cp2_wa', '6287720201177')
+@section('cp2_name', 'Jarwo 2')
