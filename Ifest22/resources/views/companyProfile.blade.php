a06f1449b1666a092703cbd0b9e17fb84114030f
rakhashafa <rakhashafa77@gmail.com>
2022-07-20 17:33:20 +0700

    company profile sisa dikit

--- Ifest22/resources/views/companyProfile.blade.php
+++ Ifest22/resources/views/companyProfile.blade.php
@@ -17,8 +17,11 @@
 @section('content')
 <body style="background-color: #1c1c1c;">
     <div class="container" style="padding: 100px 0 50px 0;">
-        <div class="row justify-content-center">
+        <div class="row justify-content-center" style="padding: 5%">
             <img src="{{asset('img/company_profile.png')}}">
+            <div style="padding: 10px 50px 0 50px; font-size: 16px; text-align:center;">
+                <p class="text-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </div>
             {{-- <div class="col-sm-5" style="padding: 20px 0 20px 0; text-align:center;">
                 <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAGFBMVEXBw8Ta183y8e74+Pm9v8Dp5+H///+5u7zLiu2GAAABH0lEQVR4nO3PCQ2EABAAMX78Oz4IORGzaR10Wadb1nOb7HyG27XPdW3vcL+Pqe79Gx7LVIdhnmGfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYZ9hn2GfYd9/eB9T3d/w2ue63uG5TXY+w+l+h1EihcbqknUAAAAASUVORK5CYII=" style="width: 70%;">
             </div>
@@ -28,9 +31,7 @@
                         <h1 class="text-title" style="margin: 0 0 0 0;">iPod</h1>
                         <p class="text-desc" style="margin: 0 0 0 0; font-size: 20px;">Informatics Festival<br>Podcast</p>
                     </div>
-                    <div style="padding: 10px 50px 0 50px; font-size: 16px; text-align:left;">
-                        <p class="text-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                    </div>
+
                 </div>
             </div> --}}
         </div>
